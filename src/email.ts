dd2652dec84f831cdc1c59a8187c880d4351ba15
Andrew Li <54134293+andrewleaf22@users.noreply.github.com>
2022-03-13 20:22:37 +0800

    code: email sending functions

---
+++ src/email.ts
@@ -0,0 +1,98 @@
+// this file contains functions to send emails
+import nodemailer from "nodemailer";
+import Student from "./models/student";
+import School from "./models/school";
+import dotenv from "dotenv";
+
+// global variables
+dotenv.config();
+const [username, password]: [string, string] = [process.env.EMAIL_USERNAME || "", process.env.EMAIL_PASSWORD || ""];
+
+// email account
+let transporter = nodemailer.createTransport({
+    host: "smtp.mxhichina.com",
+    port: 465,
+    secure: true,
+    auth: { user: username, pass: password },
+});
+
+// funcs
+export const sendSchoolReceipt = async (data: School) => { // sends receipt of the form to user
+    transporter.sendMail({
+        from: `"Research Conference" <${process.env.EMAIL_USERNAME}>`, // sender address
+        to: `"Research Conference" <${process.env.EMAIL_USERNAME}>`, // list of receivers
+        subject: `New Form Submission from ${data.name}`, // Subject line
+        html: `<b>New Form Submission. Below are the contents of the form:</b>
+            <p>School Name: ${data.name}</p>
+            <p>School Code: ${data.code}</p>
+            <p>Contact: ${data.contact.name}, ${data.contact.position} (<a href="mailto:${data.contact.email}">${data.contact.email}</a>)</p>
+        `, // html body
+    }).then(() => {
+        return 1;
+    }).catch((error: Error) => {
+        console.error(error);
+        return -1;
+    });
+}
+
+export const sendSchoolCodeEmail = async (school: School) => { // sends school code to the provided email
+    transporter.sendMail({
+        from: `"Research Conference" <${process.env.EMAIL_USERNAME}>`, // sender address
+        to: `${school.contact.name} <${school.contact.email}>`, // list of receivers
+        cc: `"Research Conference" <${process.env.EMAIL_USERNAME}>`,
+        subject: "THIS ISRC - Payment Received", // Subject line
+        html: `<div style="font-family: sans-serif"><p>Dear ${school.contact.name},</p>
+        
+            <p>We have received your payment for the 2022 Tsinghua International School Research Conference. Your students can now register. Your school code for registration purposes is</p>
+            <b style="font-size: 4rem, font-weight: 800">${school.code}</b>
+            <p>Feel free to reach out if you have any questions!</p>
+            
+            <p>Sincerely,<br/>Tsinghua International School</p></div>
+        `, // html body
+    }).then(() => {
+        return 1;
+    }).catch((error: Error) => {
+        onError(error, new Date(), school.contact.email);
+        return -1;
+    });
+}
+
+export const sendRegistrationEmail = async (student: Student) => { // sends registration confirmation to the student
+    transporter.sendMail({
+        from: `"Research Conference" <${process.env.EMAIL_USERNAME}>`, // sender address
+        to: `${student.identity.name} <${student.identity.email}>`, // list of receivers
+        cc: `"Research Conference" <${process.env.EMAIL_USERNAME}>`,
+        subject: "THIS ISRC - Registration Confirmation", // Subject line
+        html: `<p>Dear ${student.identity.name},</p>
+            <p>We have received your submission for the 2022 Tsinghua International School Research Conference. If you have any questions or would like to make any changes, please respond to this email!</p>
+            <p>Sincerely,<br/>Tsinghua International School</p>
+        `, // html body
+    }).then(() => {
+        return 1;
+    }).catch((error: Error) => {
+        onError(error, new Date(), student.identity.email);
+        return -1;
+    });
+}
+
+const onError = async (error: Error, timestamp: Date, targetEmail: string) => {
+    transporter.sendMail({
+        from: `"Research Conference" <${process.env.EMAIL_USERNAME}>`, // sender address
+        to: `Andrew Li <andrew_li22@this.edu.cn>`,
+        subject: "THIS ISRC API Error", // Subject line
+        html: `
+            <b>${error}</b> occured at <b>${timestamp.toISOString}</b>. Original email was to be sent to ${targetEmail}.
+        `, // html body
+    }).then(() => {
+        return 1;
+    }).catch((e: Error) => {
+        console.error(e);
+        return -1;
+    });
+}
+
+module.exports = {
+    sendSchoolReceipt: sendSchoolReceipt,
+    sendSchoolCodeEmail: sendSchoolCodeEmail,
+    sendRegistrationEmail: sendRegistrationEmail
+}
